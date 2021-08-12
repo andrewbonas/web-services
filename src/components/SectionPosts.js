bb54470250103aed912a60bc0c5c83a134eb6eaa
Stackbit Code Editor <projects@stackbit.com>
2021-08-12 02:11:27 +0000

    SectionPosts.js: changed

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -10,7 +10,7 @@ export default class SectionPosts extends React.Component {
         let display_posts = _.orderBy(getPages(this.props.pageContext.pages, '/blog'), 'frontmatter.date', 'desc');
         let recent_posts = display_posts.slice(0, 3);
         return (
-            <section id={_.get(section, 'section_id', null)} className={'block posts-block bg-' + _.get(section, 'background', null) + ' outer'}>
+            <section >
        
             </section>
         );

a88a15d35e2302f6038d86b58c17e853532f5205
Stackbit Code Editor <projects@stackbit.com>
2021-08-16 18:10:35 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -20,6 +20,16 @@ export default class SectionHero extends React.Component {
                     {_.get(section, 'title', null) && (
                     <h2 className="block-title underline">{_.get(section, 'title', null)}</h2>
                     )}
+                    <div className="site-branding">
+                    {_.get(this.props, 'pageContext.site.siteMetadata.header.logo_img', null) && (
+                    <p className="site-logo"><Link to={withPrefix('/')}><img src={withPrefix(_.get(this.props, 'pageContext.site.siteMetadata.header.logo_img', null))} alt={_.get(this.props, 'pageContext.site.siteMetadata.header.logo_img_alt', null)} /></Link></p>
+                    )}
+                    {((_.get(this.props, 'pageContext.frontmatter.template', null) === 'landing') || (_.get(this.props, 'pageContext.frontmatter.template', null) === 'blog')) ? (
+                    <h1 className={classNames('site-title', {'screen-reader-text': _.get(this.props, 'pageContext.site.siteMetadata.header.logo_img', null)})}><Link to={withPrefix('/')}>{_.get(this.props, 'pageContext.site.siteMetadata.title', null)}</Link></h1>
+                    ) : 
+                    <p className={classNames('site-title', {'screen-reader-text': _.get(this.props, 'pageContext.site.siteMetadata.header.logo_img', null)})}><Link to={withPrefix('/')}>{_.get(this.props, 'pageContext.site.siteMetadata.title', null)}</Link></p>
+                    }
+                  </div>
                     <div className="block-copy">
                       {markdownify(_.get(section, 'content', null))}
                     </div>

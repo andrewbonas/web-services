d51791b3089ca40c7d69d7e1c8c85381755132fe
andrewbonas <65621995+andrewbonas@users.noreply.github.com>
2022-02-13 11:36:37 -0800

    site map

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,21 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+
+ exports.onCreatePage = async ({ page, actions: { deletePage } }) => {
+  if (page.path.match(/^\/about/)) {
+    deletePage(page)
+  }
+  if (page.path.match(/^\/terms-of-service/)) {
+    deletePage(page)
+  }
+  if (page.path.match(/^\/signup/)) {
+    deletePage(page)
+  }
+  if (page.path.match(/^\/style-guide/)) {
+    deletePage(page)
+  }
+  if (page.path.match(/^\/privacy-policy/)) {
+    deletePage(page)
+  }
+}
